1b7f71de1cbd91f86717d316e38a18aee4afabfc
Justin Tsang <justin.b.tsang@gmail.com>
2016-10-06 23:57:20 +0000

    adding custom sorting assignment

---
+++ advsorting.js
@@ -0,0 +1,22 @@
+// var myArray = [1, 2, 5, 9, 10]
+
+// myArray.sort(function(a,b){
+//   return a - b
+// })
+
+// console.log(myArray)
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+students.sort(function(a,b) {
+ if (a.name > b.name) {
+  return 1;
+ } else if (a.age < b.age) {
+  return 1;
+}});
+
+
+console.log(students)
